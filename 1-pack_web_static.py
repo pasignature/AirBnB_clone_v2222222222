e960476ee685526f12d33955104b3a4ab275cff1
pasignature <pasignature@gmail.com>
2020-08-17 20:37:22 +0000

    creates pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from the contents of the web_static """
+import os
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
+        return None
